deb0e638e00138e2961c6b720333d46f4d8d8243
Kev++ <vimagick@gmail.com>
2016-05-31 10:11:23 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+alarmpi-image
+=============
+ 
+[![](https://travis-ci.org/EasyPi/alarmpi-image.svg)](https://travis-ci.org/EasyPi/alarmpi-image)
+
+Build ArchLinuxARM Image for RaspberryPi
+
+Name                               | Info
+---------------------------------- | -------------------
+[ArchLinuxARM-rpi-latest][1]       | [raspberry-pi][3]
+[ArchLinuxARM-rpi-2-latest][2]     | [raspberry-pi-2][4]
+
+[1]: #
+[2]: #
+[3]: http://archlinuxarm.org/platforms/armv6/raspberry-pi
+[4]: http://archlinuxarm.org/platforms/armv7/broadcom/raspberry-pi-2
---
+++ build.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+#
+# make alarmpi image
+#
+# - http://archlinuxarm.org/platforms/armv6/raspberry-pi
+# - http://archlinuxarm.org/platforms/armv7/broadcom/raspberry-pi-2
+#
+
+set -xe
+
+NAME=${1:-ArchLinuxARM-rpi-latest}
+#NAME=ArchLinuxARM-rpi-2-latest
+
+echo '################## download image #######################'
+
+wget http://archlinuxarm.org/os/${NAME}.tar.gz
+
+if ! curl -sSL http://archlinuxarm.org/os/${NAME}.tar.gz.md5 | md5sum -c
+then
+    exit 1
+fi
+
+echo '################## install softwares ####################'
+
+which mkfs.vfat || apt install -y bsdtar dosfstools
+
+echo '################## make partitions ######################'
+
+# 1G   == 1073741824B == 2097152S
+# 100M == 104857600B  == 204800S
+
+# Method 1
+dd if=/dev/zero of=${NAME}.img bs=1024 count=$((1024*1024))
+
+# Method 2
+#fallocate -l 1G ${NAME}.img
+
+# Method 1
+fdisk ${NAME}.img <<EOF
+n
+p
+ 
+ 
++100M
+t
+c
+n
+p
+ 
+ 
+ 
+w
+EOF
+
+# Method 2
+#parted --script ${NAME}.img \
+#    unit S \
+#    mklabel msdos \
+#    mkpart primary fat32 2048 206847 \
+#    mkpart primary ext4 206848 2097151
+
+# Method 3 (alpha)
+# sfdisk -d ${NAME}.img > alarmpi.out
+# sfdisk -f ${NAME}.img < alarmpi.out
+
+echo '################## make filesystems #####################'
+ 
+LOOP=$(losetup -f --show ${NAME}.img)
+partx -s ${LOOP}
+partx -a ${LOOP}
+ 
+mkfs.vfat -v -I ${LOOP}p1
+mkfs.ext4 -v ${LOOP}p2
+
+mkdir boot root
+mount -v -t vfat ${LOOP}p1 boot
+mount -v -t ext4 ${LOOP}p2 root
+
+echo '################## copy files ###########################'
+ 
+bsdtar -xpf ${NAME}.tar.gz -C root
+sync
+mv root/boot/* boot
+
+umount boot root
+rmdir boot root
+partx -d ${LOOP}
+losetup -d ${LOOP}
+
+echo '################## make image ###########################'
+
+gzip ${NAME}.img
+chmod a+rw ${NAME}.img.gz
+
+echo '################## done #################################'
