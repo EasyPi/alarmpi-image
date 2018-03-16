4b6b19c2696a387c7d3708bf47bdd06e35704945
kev <vimagick@gmail.com>
2018-03-16 21:09:22 +0800

    run tar in verbose mode

--- build.sh
+++ build.sh
@@ -80,7 +80,7 @@ mount -v -t ext4 ${LOOP}p2 root
 
 echo '################## copy files ###########################'
  
-tar xzf ${NAME}.tar.gz -C root >/dev/null 2>&1
+tar xvzf ${NAME}.tar.gz -C root
 sync
 mv root/boot/* boot
 
